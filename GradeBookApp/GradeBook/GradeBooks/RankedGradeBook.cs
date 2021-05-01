84e85309783af68e955689e124f22e946cf551ea
jpragasa <jpragasa@seu.edu>
2021-05-01 12:00:25 -0400

    Overrides completed

--- GradeBookApp/GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBookApp/GradeBook/GradeBooks/RankedGradeBook.cs
@@ -28,5 +28,29 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            if(Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }           
+        }
     }
 }
